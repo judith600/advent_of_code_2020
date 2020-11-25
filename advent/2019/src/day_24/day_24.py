b3f387618bdf1b0974a3569ba24d30a8425886f7
judith600 <judith.hurzlmeier@cas.de>
2020-11-25 13:58:14 +0000

    wip

--- advent/2019/src/day_24/day_24.py
+++ advent/2019/src/day_24/day_24.py
@@ -1,10 +1,19 @@
 import sys
 sys.path.append('/workspace/advent_of_code_2020/advent/2019/src')
 import file_opener
+from ErisType import State
+
+class MatrixBuilder():
+
+    def __init__(self):
+        self.matrix = [['+' for x in range (5)] for k in range(5)]
+
+    def fillInitialMatrix(self):
+        pass
 
 
 if __name__ == "__main__":
-    for elem in sys.path:
-        print(elem)
-    file = file_opener.FileOpener()
-    file.openInputFile(2019, 'input.txt')
+    builder = MatrixBuilder()
+    for line in builder.matrix:
+        for col in line:
+            print(col)
